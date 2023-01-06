1c46cee77794eba5a47ac63500c558a0013da029
jaehoon <tgc04042@cu.ac.kr>
2023-01-06 20:08:05 +0900

    complete statefulwidget example

--- statefulwidget/lib/screen/home_screen.dart
+++ statefulwidget/lib/screen/home_screen.dart
@@ -1,25 +1,75 @@
 import 'package:flutter/material.dart';
 
-
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   final Color color;
 
-  const HomeScreen({
+  HomeScreen({
     required this.color,
     Key? key,
-}) : super(key: key);
+  }) : super(key: key) {
+    print('Widget Constructor 실행!');
+  }
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    print('createState 실행!');
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print('initState 실행!');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    print('didChangeDependencies 실행!');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    print('deactivate 실행!');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose 실행!');
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget 실행!');
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      color: widget.color, // widget.color : HomeScreen 위젯에 있는 color 값을 가져옴
+    print('build 실행!');
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          number ++;
+        });
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color, // widget.color : HomeScreen 위젯에 있는 color 값을 가져옴
+        child: Center(
+          child: Text(number.toString()),
+        ),
+      ),
     );
   }
 }
